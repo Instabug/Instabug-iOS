d553c5928738aa3ace0fb578d2fa5912482a3fa5
Moataz Soliman <msoliman3890@gmail.com>
2014-10-03 15:43:54 +0200

    Remove the add build script

--- add_instabug_build_script.rb
+++ add_instabug_build_script.rb
@@ -1,35 +1,35 @@
-require 'xcodeproj'
+# require 'xcodeproj'
 
-INSTABUG_PHASE_NAME = "Upload Instabug dSYM"
-INSTABUG_PHASE_SCRIPT = <<-SCRIPTEND
-  # SKIP_SIMULATOR_BUILDS=1
-  SCRIPT_SRC=$(find "$PROJECT_DIR" -name 'Instabug_dsym_upload.sh')
-  if [ ! "${SCRIPT_SRC}" ]; then
-    echo "Instabug: err: script not found. Make sure that you're including Instabug.bundle in your project directory"
-    exit 1
-  fi
-  source "${SCRIPT_SRC}"
-  SCRIPTEND
+# INSTABUG_PHASE_NAME = "Upload Instabug dSYM"
+# INSTABUG_PHASE_SCRIPT = <<-SCRIPTEND
+#   # SKIP_SIMULATOR_BUILDS=1
+#   SCRIPT_SRC=$(find "$PROJECT_DIR" -name 'Instabug_dsym_upload.sh')
+#   if [ ! "${SCRIPT_SRC}" ]; then
+#     echo "Instabug: err: script not found. Make sure that you're including Instabug.bundle in your project directory"
+#     exit 1
+#   fi
+#   source "${SCRIPT_SRC}"
+#   SCRIPTEND
   
-current_path = nil
-while current_path != Dir.pwd
-  current_path = Dir.pwd
-  project_path = Dir.glob("#{current_path}/*.xcodeproj")
+# current_path = nil
+# while current_path != Dir.pwd
+#   current_path = Dir.pwd
+#   project_path = Dir.glob("#{current_path}/*.xcodeproj")
   
-  unless project_path.first.nil?
-    project = Xcodeproj::Project.open(project_path.first)
-    targets = project.targets
+#   unless project_path.first.nil?
+#     project = Xcodeproj::Project.open(project_path.first)
+#     targets = project.targets
     
-    targets.each do |target|
-      if target.product_type == "com.apple.product-type.application" or target.product_type == "com.apple.product-type.app-extension"
-        phase = target.new_shell_script_build_phase(INSTABUG_PHASE_NAME)
-        phase.shell_script = INSTABUG_PHASE_SCRIPT
-        project.save()
-      end
-    end
+#     targets.each do |target|
+#       if target.product_type == "com.apple.product-type.application" or target.product_type == "com.apple.product-type.app-extension"
+#         phase = target.new_shell_script_build_phase(INSTABUG_PHASE_NAME)
+#         phase.shell_script = INSTABUG_PHASE_SCRIPT
+#         project.save()
+#       end
+#     end
     
-    break
-  end
+#     break
+#   end
 
-  Dir.chdir("..")
-end
+#   Dir.chdir("..")
+# end
