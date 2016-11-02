f8cf35e55505e2d9ee026ca323d74f91cf6a82bf
alliamin <aamin@instabug.com>
2016-11-02 07:42:08 +0200

    v2.0.0

--- Instabug.framework/Headers/IBGEnums.h
+++ Instabug.framework/Headers/IBGEnums.h
@@ -40,13 +40,26 @@ typedef NS_ENUM(NSInteger, IBGColorTheme) {
  */
 typedef NS_ENUM(NSInteger, IBGInvocationMode) {
     IBGInvocationModeNA,
-    IBGInvocationModeBugReporter,
-    IBGInvocationModeFeedbackSender
+    IBGInvocationModeBugReporter __attribute__((deprecated)),
+    IBGInvocationModeFeedbackSender __attribute__((deprecated)),
+    IBGInvocationModeNewBug,
+    IBGInvocationModeNewFeedback,
+    IBGInvocationModeNewChat,
+    IBGInvocationModeChatsList
+};
+
+/**
+ *  Type of report to be submit
+ */
+typedef NS_ENUM(NSInteger, IBGReportType) {
+    IBGReportTypeBug,
+    IBGReportTypeFeedback
 };
 
 /**
  *  Type of feedback to be submit
  */
+__attribute__((deprecated))
 typedef NS_ENUM(NSInteger, IBGFeedbackType) {
     IBGFeedbackTypeBug,
     IBGFeedbackTypeFeedback,
@@ -56,6 +69,7 @@ typedef NS_ENUM(NSInteger, IBGFeedbackType) {
 /**
  *  State of Issue after SDK dismiss
  */
+__attribute__((deprecated))
 typedef NS_ENUM(NSInteger, IBGIssueState) {
     // Issue is submitted
     IBGIssueSubmitted,
@@ -66,11 +80,24 @@ typedef NS_ENUM(NSInteger, IBGIssueState) {
 };
 
 /**
+ *  Type of SDK dismiss
+ */
+typedef NS_ENUM(NSInteger, IBGDismissType) {
+    // Dismissed after report submit
+    IBGDismissTypeSubmit,
+    // Dismissed via cancel
+    IBGDismissTypeCancel,
+    // Dismissed while taking screenshot
+    IBGDismissTypeAddAttachment
+};
+
+/**
  *  The supported locales
  */
 typedef NS_ENUM(NSInteger, IBGLocale) {
     IBGLocaleArabic,
     IBGLocaleChineseSimplified,
+    IBGLocaleChineseTaiwan,
     IBGLocaleChineseTraditional,
     IBGLocaleCzech,
     IBGLocaleDanish,
@@ -81,10 +108,88 @@ typedef NS_ENUM(NSInteger, IBGLocale) {
     IBGLocaleJapanese,
     IBGLocaleKorean,
     IBGLocalePolish,
+    IBGLocalePortugese,
     IBGLocalePortugueseBrazil,
     IBGLocaleRussian,
+    IBGLocaleSlovak,
     IBGLocaleSpanish,
     IBGLocaleSwedish,
     IBGLocaleTurkish
+};
+
+/**
+ *  Instabug strings
+ */
+typedef NS_ENUM(NSInteger, IBGString) {
+    //"<Shake> your device to talk to us"
+    IBGStringShakeHint,
+    //"<Swipe with 2 fingers> to talk to us"
+    IBGStringSwipeHint,
+    //"<Swipe from the edge> to talk to us"
+    IBGStringEdgeSwipeStartHint,
+    //"We love to hear your feedback"
+    IBGStringStartAlertText,
+    //"Please enter a valid email"
+    IBGStringInvalidEmailMessage,
+    //"Invalid Email"
+    IBGStringInvalidEmailTitle,
+    //"Please enter a valid comment"
+    IBGStringInvalidCommentMessage,
+    //"Invalid Comment"
+    IBGStringInvalidCommentTitle,
+    //"Help & Feedback"
+    IBGStringInvocationHeader,
+    //"Talk to us"
+    IBGStringTalkToUs,
+    //"Report bug"
+    IBGStringReportBug,
+    //"Suggest an Improvement"
+    IBGStringReportFeedback,
+    //"Enter your Email"
+    IBGStringEmailFieldHint,
+    //"What went wrong?"
+    IBGStringCommentFieldHintForBugReport,
+    //"How can we improve?"
+    IBGStringCommentFieldHintForFeedback,
+    //"Take a screen recording Note"
+    IBGStringAddScreenRecordingMessage,
+    //"Record a Voice Note"
+    IBGStringAddVoiceMessage,
+    //"Select Image from Gallery"
+    IBGStringAddImageFromGallery,
+    //"Take a Screenshot"
+    IBGStringAddExtraScreenshot,
+    //"Microphone Access Denied"
+    IBGStringAudioRecordingPermissionDeniedTitle,
+    //"You can enable access in Privacy Settings"
+    IBGStringAudioRecordingPermissionDeniedMessage,
+    //"Settings"
+    IBGStringMicrophonePermissionAlertSettingsButtonTitle,
+    //"Conversations"
+    IBGStringChatsHeaderTitle,
+    //"%@ Team"
+    IBGStringTeam,
+    //"Press and Hold to Record"
+    IBGStringRecordingMessageToHoldText,
+    //"Release to Attach"
+    IBGStringRecordingMessageToReleaseText,
+    //"%@ new messages from %@"
+    IBGStringMessagesNotification,
+    //%@ new messages from %@ and others
+    IBGStringMessagesNotificationAndOthers,
+    //"Draw on screenshot"
+    IBGStringScreenshotHeaderTitle,
+    //"OK"
+    IBGStringOkButtonTitle,
+    //"Cancel"
+    IBGStringCancelButtonTitle,
+    //"Thank you"
+    IBGStringThankYouText,
+    //"Audio"
+    IBGStringAudio,
+    //"Screen Recording"
+    IBGStringScreenRecording,
+    //"Image"
+    IBGStringImage
 };
 //===========================================================================================================================================
