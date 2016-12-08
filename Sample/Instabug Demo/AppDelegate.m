c8fae949bfe3fac121c498b6f5883f022ba5f166
Hesham Abd-Elmegid <hmegid@instabug.com>
2016-12-08 19:18:34 +0200

    v6.1.3

--- Sample/Instabug Demo/AppDelegate.m
+++ Sample/Instabug Demo/AppDelegate.m
@@ -12,7 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Add your app token below. You can find the token at https://instabug.com/app/sdk/
-    [Instabug startWithToken:@"" invocationEvent:IBGInvocationEventShake];
+    [Instabug startWithToken:<#app token#> invocationEvent:IBGInvocationEventShake];
     return YES;
 }
 
