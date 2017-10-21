87f4e949222e55925a9dd19acdb49a20d7e8bc4c
Aly <aly.yakan@gmail.com>
2017-10-21 15:20:15 +0200

    Removed log file from dsInstabug_dsym_strip.sh

--- Instabug.framework/Instabug.bundle/Instabug_dsym_strip.sh
+++ Instabug.framework/Instabug.bundle/Instabug_dsym_strip.sh
@@ -1,49 +1,39 @@
 cd "${ARCHIVE_DSYMS_PATH}"
 
-LOG_FILE="Instabug_dsym_strip_invalid_archs_log.txt"
-
-echo "Instabug: Started post-archiving dsym strip script" >> $LOG_FILE
-echo "" >> $LOG_FILE
-echo "Path to archive ${ARCHIVE_PATH}" >> $LOG_FILE
-echo "Path to dsyms ${ARCHIVE_DSYMS_PATH}" >> $LOG_FILE
-echo "" >> $LOG_FILE
+echo "Instabug: Started post-archiving dsym strip script"
+echo "Instabug: Path to archive ${ARCHIVE_PATH}"
+echo "Instabug: Path to dsyms ${ARCHIVE_DSYMS_PATH}"
 
 binary="${DWARF_DSYM_FOLDER_PATH}/Instabug.framework.dSYM/Contents/Resources/DWARF/Instabug"
 
-echo "Valid architectures: ${VALID_ARCHS}" >> $LOG_FILE
-echo "Wanted architectures: ${ARCHS}" >> $LOG_FILE
-echo "" >> $LOG_FILE
+echo "Instabug: Valid architectures: ${VALID_ARCHS}"
+echo "Instabug: Architectures: ${ARCHS}"
   
 # Get architectures for current file
 archs="$(lipo -info "$binary" | rev | cut -d ':' -f1 | rev)"
 stripped=""
-echo "Retrieved Architectures: $archs" >> $LOG_FILE
-echo "" >> $LOG_FILE
+echo "Instabug: Instabug's Architectures: $archs"
 
 # Loop on architectures and strip those that are not in $ARCHS
 for arch in $archs; do
 
   if ! [[ "${ARCHS}" == *"$arch"* ]]; then
-    echo "Stripping architecture: $arch" >> $LOG_FILE
-    echo "" >> $LOG_FILE
+    echo "Instabug: Stripping architecture: $arch"
 
     # Strip non-valid architectures in-place
     output_path="${ARCHIVE_DSYMS_PATH}/Instabug.framework.dSYM/Contents/Resources/DWARF/Instabug"
     lipo -remove "$arch" -output "$output_path" "$output_path"
     stripped="$stripped $arch"
   else
-    echo "Architecture $arch was not stripped" >> $LOG_FILE
-    echo "" >> $LOG_FILE
+    echo "Instabug: Architecture $arch was not stripped"
   fi
 
 done
 
 if [[ "$stripped" ]]; then
-  echo "Stripped $binary of architectures:$stripped" >> $LOG_FILE
-  echo "" >> $LOG_FILE
+    echo "Instabug: Stripped $binary of architectures:$stripped"
 else
-  echo "Did not find anything to strip" >> $LOG_FILE
-  echo "" >> $LOG_FILE
+    echo "Instabug: Did not find anything to strip"
 fi
 
 echo "Instabug: Finished post-archiving dsym strip script" >> $LOG_FILE
