df467b0cb02d3f6008ddce1afd2dc2ff2a45858c
Aly <aly.yakan@gmail.com>
2018-07-05 15:40:30 +0200

    Updates Instabug API calls

--- Sample/Instabug Demo/AppDelegate.swift
+++ Sample/Instabug Demo/AppDelegate.swift
@@ -11,12 +11,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     let appToken = "81fb5c82a5d7144eb77da6e1ec9d60e6"
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
-        
-        // Add your app token below. You can find the token at https://instabug.com/app/sdk/
-        Instabug.start(withToken: appToken, invocationEvent: .floatingButton)
-        Instabug.setAutoScreenRecordingEnabled(true)
-        Instabug.setNetworkLoggingEnabled(true)
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        Instabug.start(withToken: appToken, invocationEvents: .floatingButton)
+        Instabug.autoScreenRecordingEnabled = true
+        NetworkLogger.enabled = true
         return true
     }
     
