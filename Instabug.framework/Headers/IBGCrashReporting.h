46851b1fc48ef908bd2ca1965db9d34c5880a825
Kmohamed <khaled.elmorabea@gmail.com>
2020-11-25 17:49:01 +0200

    Create V10.1

--- Instabug.framework/Headers/IBGCrashReporting.h
+++ Instabug.framework/Headers/IBGCrashReporting.h
@@ -5,7 +5,7 @@
  
  Copyright:  (c) 2013-2019 by Instabug, Inc., all rights reserved.
  
- Version:    10.0.3
+ Version:    10.1
  */
 
 #import <Foundation/Foundation.h>
@@ -14,6 +14,13 @@ NS_SWIFT_NAME(CrashReporting)
 @interface IBGCrashReporting : NSObject
 
 @property (class, atomic, assign) BOOL enabled;
+
+/**
+ @brief Enable/Disable out of memory crash reporting.
+ 
+ Out of memory crash reporting is enabled by default. OOM will be disabled if crash reporting is disabled.
+ */
+@property (class, atomic, assign) BOOL OOMEnabled;
 
 /**
  @brief Report an exception manually.
